6ded042d7adb6f0ea582d079167f95b126fbe695
kazik1991 <kuba113@hotmail.com>
2015-05-20 16:09:32 +0200

    Dodanie kalkulatora

---
+++ kalkulator.py
@@ -0,0 +1,15 @@
+def dodawanie(a,b):
+	return a+b
+	
+def get_info():
+	print("To jest prosty kalkulator")
+
+get_info()
+
+print('Podaj pierwsza liczbe')
+a = int(input())
+print('Podaj druga liczbe')
+b = int(input())
+
+print(dodawanie(a,b))
+
